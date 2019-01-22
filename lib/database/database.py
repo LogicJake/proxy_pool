2d1068a452f3e47618f9bd72d9bec34304d8bd5c
Chenyang Shi <chenyang9608@qq.com>
2019-01-22 19:57:33 +0800

    update basic function

--- lib/database/database.py
+++ lib/database/database.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author: LogicJake
 # @Date:   2019-01-16 13:21:21
-# @Last Modified time: 2019-01-16 17:18:23
+# @Last Modified time: 2019-01-22 19:55:42
 from abc import ABCMeta, abstractmethod
 
 
@@ -12,17 +12,29 @@ class DataBase(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def is_table_exist(self):
+    def close(self):
         pass
 
     @abstractmethod
-    def select(self):
+    def update(self, table_name, key_values, where):
+        pass
+
+    @abstractmethod
+    def insert(self, table_name, key_values):
+        pass
+
+    @abstractmethod
+    def is_table_exist(self, table_name):
+        pass
+
+    @abstractmethod
+    def select(self, table_name, column_name, where=None, limit=None, order_by=None, order='desc'):
+        pass
+
+    @abstractmethod
+    def delete(self, table_name, where=None):
         pass
 
     @abstractmethod
     def create_required_tables(self):
-        pass
-
-    @abstractmethod
-    def close(self):
         pass
